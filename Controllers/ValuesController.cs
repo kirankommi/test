509d697995fc8896821f9013dc4e543078bb3485
kirankommi <kirankumar.kommi@gmail.com>
2019-10-01 19:48:12 +0530

    test

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace test.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value5", "value6" };
         }
 
         // GET api/values/5
